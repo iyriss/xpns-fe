56ca45fe97a71662682577b36f9143ea376e865b
Iyris Vigil <iyrisvigil@gmail.com>
2025-06-20 21:53:19 -0700

    modify remix config

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  future: {
+    v3_routeConvention: true,
+  },
   serverBuildTarget: 'vercel',
-  server: './server.js',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['.*'],
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverDependenciesToBundle: 'all',
 };
